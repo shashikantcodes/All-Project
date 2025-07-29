000e7d29f861a894768669e9b64e3d123e2d95c4
Shashikant Pathak <shashikant.pathak.dev@gmail.com>
2025-07-29 20:10:26 +0530

    Update Home.jsx

--- React/zeptro/src/pages/Home.jsx
+++ React/zeptro/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Carousel from '../components/carousel'
+import Carousel from '../components/Carousel'
 import MidBanner from '../components/MidBanner'
 import Features from '../components/Features'
 
